4d7c1b7e17585a5cb6b84cc9d31676e06b95526b
derpadoo <thomab310@gmail.com>
2019-01-17 12:21:41 -0600

    Updated token.py to comply with PEP-8

--- token.py
+++ token.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import string, hashlib
-from random import *
-allchar = string.ascii_letters + string.digits
-password = "".join(choice(allchar) for x in range(randint(12, 12)))
-print('Token: '  + password)
-tokenhash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+import hashlib
+import random
+import string
 
-with open('/root/token.sha256', 'w') as f:
-	f.write(tokenhash)
+allchar = string.ascii_letters + string.digits
+password = "".join(random.choice(allchar) for x in range(random.randint(12, 12)))
+print("Token: " + password)
+tokenhash = hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+with open("/root/token.sha256", "w") as f:
+    f.write(tokenhash)
